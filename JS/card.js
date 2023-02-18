608e2803c4e565ac928abecd21c3996641c5879f
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-02-18 22:10:23 +0600

    add function for all the section

--- JS/card.js
+++ JS/card.js
@@ -22,10 +22,9 @@ function displayTheValue(inputName, totalValue) {
     section.appendChild(tr);
 }
 
-// 
 let number = 0;
 
-// function for triangle seciton
+//  1.function for triangle seciton
 document.getElementById('btn-triangle').addEventListener('click', function () {
     const getTriangleTitle = document.getElementById('triangle-title').innerText;
     const triangleInputFirst = getElementByIdInput('triangle-input-one');
@@ -36,6 +35,77 @@ document.getElementById('btn-triangle').addEventListener('click', function () {
         return;
     }
     number += 1
-    const mutiInput = 0.5 * triangleInputFirst * triangleInputSecond;
-    displayTheValue(getTriangleTitle, mutiInput.toFixed(2))
+    const inputMultification = 0.5 * triangleInputFirst * triangleInputSecond;
+    displayTheValue(getTriangleTitle, inputMultification.toFixed(2))
+})
+
+// 2.function for Rectangle seciton
+document.getElementById('btn-rectangle').addEventListener('click', function () {
+    const getRectangleTitle = document.getElementById('rectangle-title').innerText;
+    const rectangleInputFirst = getElementByIdInput('rectangle-input-field-one');
+    const rectangleInputSecond = getElementByIdInput('rectangle-input-field-two');
+    if (isNaN(rectangleInputFirst) || isNaN(rectangleInputSecond) || rectangleInputFirst <= 0 ||
+        rectangleInputSecond <= 0) {
+        alert('Please Enter A Valid Number');
+        return;
+    }
+    number += 1
+    const inputMultification = 0.5 * rectangleInputFirst * rectangleInputSecond;
+    displayTheValue(getRectangleTitle, inputMultification.toFixed(2))
+})
+// 3.function for Parallelogram seciton
+document.getElementById('btn-parallelogram').addEventListener('click', function () {
+    const getParallelogramTitle = document.getElementById('parallelogram-title').innerText;
+    const ParallelogramInputFirst = getElementByIdInput('parallelogram-input-one');
+    const ParallelogramInputSecond = getElementByIdInput('parallelogram-input-two');
+    if (isNaN(ParallelogramInputFirst) || isNaN(ParallelogramInputSecond) || ParallelogramInputFirst <= 0 ||
+        ParallelogramInputSecond <= 0) {
+        alert('Please Enter A Valid Number');
+        return;
+    }
+    number += 1
+    const inputMultification = 0.5 * ParallelogramInputFirst * ParallelogramInputSecond;
+    displayTheValue(getParallelogramTitle, inputMultification.toFixed(2))
+})
+// 4.function for Rhombus seciton
+document.getElementById('btn-rhombus').addEventListener('click', function () {
+    const getRhombusTitle = document.getElementById('rhombus-title').innerText;
+    const rhombusInputFirst = getElementByIdInput('rhombus-input-field-one');
+    const rhombusInputSecond = getElementByIdInput('rhombus-input-field-two');
+    if (isNaN(rhombusInputFirst) || isNaN(rhombusInputSecond) || rhombusInputFirst <= 0 ||
+        rhombusInputSecond <= 0) {
+        alert('Please Enter A Valid Number');
+        return;
+    }
+    number += 1
+    const inputMultification = 0.5 * rhombusInputFirst * rhombusInputSecond;
+    displayTheValue(getRhombusTitle, inputMultification.toFixed(2))
+})
+// 5.function for Pentagon seciton
+document.getElementById('btn-pentagon').addEventListener('click', function () {
+    const getPentagonTitle = document.getElementById('pentagon-title').innerText;
+    const PentagonInputFirst = getElementByIdInput('pentagon-input-field-one');
+    const PentagonInputSecond = getElementByIdInput('pentagon-input-field-two');
+    if (isNaN(PentagonInputFirst) || isNaN(PentagonInputSecond) || PentagonInputFirst <= 0 ||
+        PentagonInputSecond <= 0) {
+        alert('Please Enter A Valid Number');
+        return;
+    }
+    number += 1
+    const inputMultification = 0.5 * PentagonInputFirst * PentagonInputSecond;
+    displayTheValue(getPentagonTitle, inputMultification.toFixed(2))
+})
+// 6.function for Ellipse seciton
+document.getElementById('btn-ellipse').addEventListener('click', function () {
+    const getEllipseTitle = document.getElementById('ellipse-title').innerText;
+    const ellipseInputFirst = getElementByIdInput('ellipse-input-field-one');
+    const ellipseInputSecond = getElementByIdInput('ellipse-input-field-two');
+    if (isNaN(ellipseInputFirst) || isNaN(ellipseInputSecond) || ellipseInputFirst <= 0 ||
+        ellipseInputSecond <= 0) {
+        alert('Please Enter A Valid Number');
+        return;
+    }
+    number += 1
+    const inputMultification = 0.5 * ellipseInputFirst * ellipseInputSecond;
+    displayTheValue(getEllipseTitle, inputMultification.toFixed(2))
 })
