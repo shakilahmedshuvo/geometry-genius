8486aea630d75037f37bd3c08b3af3b25db38354
shakilahmedshuvo <shakilahmedshuvo612@gmail.com>
2023-02-18 20:59:42 +0600

    add function for triangle seciton and some fixed

--- JS/card.js
+++ JS/card.js
@@ -1,5 +1,5 @@
 // first function
-function getElementById(idInput) {
+function getElementByIdInput(idInput) {
     const getIdinput = document.getElementById(idInput);
     const idInputString = getIdinput.value;
     const getValueStringToNumber = parseFloat(idInputString);
@@ -7,18 +7,35 @@ function getElementById(idInput) {
     return getValueStringToNumber;
 }
 
-
+// Second function 
 function displayTheValue(inputName, totalValue) {
-    const section = document.getElementById("showDataHere");
+    const section = document.getElementById('showDataHere');
     const tr = document.createElement('tr');
     tr.innerHTML = `
 
-    <td>${NO}</td>
+    <td>${number}</td>
 
     <td>${inputName}</td>
 
     <td>${totalValue}</td>
-    `
-    section.appendChild(td);
+    `;
+    section.appendChild(tr);
 }
 
+// 
+let number = 0;
+
+// function for triangle seciton
+document.getElementById('btn-triangle').addEventListener('click', function () {
+    const getTriangleTitle = document.getElementById('triangle-title').innerText;
+    const triangleInputFirst = getElementByIdInput('triangle-input-one');
+    const triangleInputSecond = getElementByIdInput('triangle-input-two');
+    if (isNaN(triangleInputFirst) || isNaN(triangleInputSecond) || triangleInputFirst <= 0 ||
+        triangleInputSecond <= 0) {
+        alert('Please Enter A Valid Number');
+        return;
+    }
+    number += 1
+    const mutiInput = 0.5 * triangleInputFirst * triangleInputSecond;
+    displayTheValue(getTriangleTitle, mutiInput.toFixed(2))
+})
